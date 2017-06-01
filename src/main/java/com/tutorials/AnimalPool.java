d62db527a67b1027f746cf34321a1b705359389e
VHOMYAK-PC\hvity <h.vitya56@gmail.com>
2017-06-01 13:02:25 +0300

    fix

--- src/main/java/com/tutorials/AnimalPool.java
+++ src/main/java/com/tutorials/AnimalPool.java
@@ -20,45 +20,32 @@ public class AnimalPool {
     }
 
     public AnimalPool(List<AnimalEntity> animals) {
-
         pool = new ArrayDeque<AnimalEntity>();
-
         this.animals = animals;
-
     }
 
     public String produce() throws InterruptedException {
 
-    synchronized (pool) {
-
-        while (pool.size() > 20) {
-            if(animals.isEmpty())
-              return "";
-            pool.wait();
-        }
-
-        ;
-
+        synchronized (pool) {
+            while (pool.size() > 20) {
+                if (animals.isEmpty())
+                    return "";
+                pool.wait();
+            }
             pool.add(animals.remove(0));
-           pool.notifyAll();
-
-
-    }
-
-        return "Added 1. Pool capacity = "+pool.size() ;
+            pool.notifyAll();
+        }
+        return "Added 1. Pool capacity = " + pool.size();
 
     }
 
     public AnimalEntity consume() throws InterruptedException {
         synchronized (pool) {
-
             while (pool.size() == 0) {
                 pool.wait();
             }
-
             AnimalEntity returnValue = pool.removeFirst();
             pool.notifyAll();
-
             return returnValue;
         }
 
